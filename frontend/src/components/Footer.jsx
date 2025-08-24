ca78e58a80ea973cd67afafdf91857fea335ca65
Lahiu-Thauka <22ug1-0453@sltc.ac.lk>
2025-08-24 16:11:37 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer></footer>
+      <div className="copyright">
+        &copy; CopyRight 2025. All Rights Reserved By LAHIRU THARUKA
+      </div>
+    </>
+  );
+};
+
+export default Footer;
