74b21de3c74e6ea74714c82d28e3dcac81746123
Lahiru-Tharuka <156454708+Lahiru-Tharuka@users.noreply.github.com>
2025-08-27 22:10:53 +0530

    feat: add images for top niches and categories

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../components/Hero";
 import TopNiches from "../components/TopNiches";
 import HowItWorks from "../components/HowItWorks";
+import JobCategories from "../components/JobCategories";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <TopNiches />
+      <JobCategories />
       <HowItWorks />
     </>
   );
