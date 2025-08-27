dd5332663d251cf3415f4ccadd8cbf9929e70f11
Lahiru-Tharuka <156454708+Lahiru-Tharuka@users.noreply.github.com>
2025-08-27 22:58:05 +0530

    Remove Featured Jobs section and style Post a Job button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import Hero from "../components/Hero";
 import TopNiches from "../components/TopNiches";
 import HowItWorks from "../components/HowItWorks";
-import FeaturedJobs from "../components/FeaturedJobs";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <FeaturedJobs />
       <TopNiches />
       <HowItWorks />
     </>
